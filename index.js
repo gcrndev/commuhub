fd8c4fe5ccf6504b5120bdec48700185c0dfc6b7
gcrndev <gabrielcosta1532006pt@gmail.com>
2026-05-25 15:15:56 +0100

    Handle background push notifications

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import { AppRegistry } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import { name as appName } from './app.json';
+import { displayPushNotification } from './src/services/notificationService';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  await displayPushNotification(remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
